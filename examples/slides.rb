842ad1dfd7701c2871e09f8550b06d23e04cb129
Nicklas Persson <n@sl33p.com>
2014-03-02 19:32:28 +0000

    Adding main sections

---
+++ examples/slides.rb
@@ -0,0 +1,87 @@
+# encoding: utf-8
+
+center <<-EOS
+  \e[31;1mBuilding Cathedrals\e[0m
+
+
+  Nicklas de León Persson
+
+  @takete
+
+  Design and Banter, March 2014
+EOS
+
+center <<-EOS
+
+"As cathedrals took decades,
+and often even centuries to complete,
+few people who worked on them expected to
+see them finished during their lifetimes."
+
+EOS
+
+center <<-EOS
+
+Instant Gratification
+
+EOS
+
+
+center <<-EOS
+
+Instant Gratification
+
+EOS
+
+center <<-EOS
+
+Short Form Reading
+
+EOS
+
+center <<-EOS
+
+Short Form Writing
+
+EOS
+
+center <<-EOS
+
+Short Form Thinking
+
+EOS
+
+
+center <<-EOS
+
+Playing the Long Game
+
+EOS
+
+center <<-EOS
+
+What's the Cathedral of Today?
+
+EOS
+
+center <<-EOS
+
+Replacing Self-flagellation
+
+EOS
+
+center <<-EOS
+
+Transitioning to No Discernable Gratification
+
+EOS
+
+section "Thank You" do
+
+center <<-EOS
+
+@takete
+
+EOS
+
+end
