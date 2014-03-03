657b9a4d826aa27feb2c4ec125bf64824ede19bb
Nicklas Persson <n@sl33p.com>
2014-03-03 16:46:00 +0000

    Last minute changes, adding some notes

--- cathedrals/slides.rb
+++ cathedrals/slides.rb
@@ -11,6 +11,9 @@ center <<-EOS
   Design and Banter, March 2014
 EOS
 
+#The people who built cathedrals knew they wouldn't be alive to see them finished. We need an ounce of that thinking back. Our preoccupation with caching out, writing (and reading) in short form, constantly moving to stay on the edge is not sustainable. We need to take a longer view.
+
+#In The Cathedral Builders (PIMLICO: London, 1993), Jean Gimpel writes
 center <<-EOS
 
 "As cathedrals took decades,
@@ -31,19 +34,20 @@ part of a process that was larger than oneself."
 
 EOS
 
+#Today, the idea of starting a project which wouldn't ship for a century seems preposterous. Our world moves much faster, and our plans are much shorter.
 center <<-EOS
 
 We're No Longer Building Cathedrals
 
 EOS
 
-
+#The web is not a place that naturally encourages contemplation and slow, methodical movement. It's ephemeral in nature, it's instant, it's changeable, it's intangible. Maybe this short-lived dragonfly-like nature of our medium is the reason we’re grasping for the truths hidden in words like ‘craft’ and ‘artisan’, words which signify heritage, lineage, care, thoughtfulness, and taking your time.
 center <<-EOS
 
-We Demand Instant Gratification
+The Short-Form Predicament
 
 EOS
-
+#We talk about these things, and we celebrate them. But when we read, our reading is short form. We need little counters telling us how many minutes the article will take to consume, and anything over 4.5 minutes will be bookmarked to read later. (How about never? Is never good for you?)
 center <<-EOS
 
 Short Form Reading
@@ -56,19 +60,36 @@ Short Form Writing
 
 EOS
 
+#When we write, our writing is in short form. We spend our time creating small, seemingly insignificant updates and comments. The problem, however, is that they're not insignificant when added up. All the little moments, all the tiny bits of effort amount to something. We choose — whether we know it or not — what this something is.
 center <<-EOS
 
 Short Form Thinking
 
 EOS
 
-
+#The kind of mind that put their efforts into building something which wouldn't be finished in their lifetime believed in rewards after death. They believed in something that was bigger than themselves.
 center <<-EOS
 
-Can We Get Back to Playing the Long Game?
+How Do We Focus on the Long Game?
 
 EOS
 
+#Is it possible for us, having replaced superstition with rational thinking and secular belief systems to find something which would provide enough motivation to make similar efforts? In other words, what’s there to be *that* excited about? What’s the cathedral of today?
+center <<-EOS
+
+What’s the Cathedral of Today?
+
+EOS
+
+#When supporting cathedral building became popular, it was replacing other, longer established ways to celebrate the glory of God and buying your way into heaven. Long, arduous pilgrimages and self-flagellation were popular options.
+
+center <<-EOS
+
+We Demand Instant Gratification
+
+EOS
+
+#The point about flagellation and long barefoot walks is that you can feel the effect straight away. The instant sensation gives gratification and a promise of having done things right. If it hurts, it must be effective. The instant gratification of getting 43 favourites and retweets is not that different; it's all about affirmation, for you, right now.
 center <<-EOS
 
 "[…] the challenge of making things that endure,
@@ -77,6 +98,15 @@ despite the incentive structure of the real-time web"
 — Alexis C. Madrigal, on Twitter
 
 EOS
+
+#We might be able to switch to another model of gratification, much like the people did then. For many,  believing in a specific cause might not even be necessary, the new way of doing things might just be an acceptable replacement for the old. But if we want to lead the way, we need to find the focus, we need to set the goals.
+center <<-EOS
+
+Replacing Self-flagellation
+
+EOS
+
+#This is not (necessarily) about saving the world. It's about getting excited about creating work which is more sustainable, which looks a little further than the horizon.
 
 center <<-EOS
 
@@ -88,25 +118,27 @@ systems that survive long enough to outlive us?"
 EOS
 
 
-center <<-EOS
-
-Replacing Self-flagellation
-
-EOS
-
-center <<-EOS
-
-Transitioning to No Discernable Gratification
-
-EOS
+#If we were to change the way we looked at our work, and imagined a timescale of decades instead of weeks, what would we change about what we do?
 
 center <<-EOS
 
 What's the Cathedral of Today?
 
 EOS
+#The motivation and single-mindedness to wake up every morning and do a little bit of work towards this goal might be all the change we need. Putting our energy into focus could make all the difference.
+center <<-EOS
 
-section "Thank You" do
+Transitioning to No Discernable Gratification
+
+EOS
+
+
+#There are questions which still require contemplation. How can you make the transition from instant gratification to no discernable gratification at all? Is it even desirable? But most importantly, what changes can we make to the world with a change in mindset?
+center <<-EOS
+
+Final Thoughts
+
+EOS
 
 center <<-EOS
 
@@ -114,4 +146,3 @@ center <<-EOS
 
 EOS
 
-end
